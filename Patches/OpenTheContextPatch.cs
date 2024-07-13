9b1e07a4b42ee9d2b6495ea557c9cd3e87bc2688
JD <JD@DESKTOP-666DDU8>
2024-07-12 20:22:28 -0700

    Add project files.

---
+++ Patches/OpenTheContextPatch.cs
@@ -0,0 +1,43 @@
+﻿using EFT.InventoryLogic;
+using EFT.UI;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using UnityEngine;
+
+
+namespace OpenTheDamnContextMenu
+{
+    public class OpenTheContext : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(ItemUiContext), nameof(ItemUiContext.ShowContextMenu));
+        }
+
+        // Use reflection to get the private fields
+        private static readonly FieldInfo inventoryControllerField = typeof(ItemUiContext).GetField("inventoryControllerClass", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly FieldInfo itemInfoInteractionsField = typeof(ItemUiContext).GetField("itemInfoInteractionsAbstractClass", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        [PatchPrefix]
+        static bool Prefix(ItemUiContext __instance, ItemContextAbstractClass itemContext, Vector2 position)
+        {
+
+            var inventoryController = (InventoryControllerClass)inventoryControllerField.GetValue(__instance);
+            var itemInfoInteractions = __instance.GetItemContextInteractions(itemContext, null);
+
+            if (itemInfoInteractionsField != null)
+            {
+                itemInfoInteractionsField.SetValue(__instance, itemInfoInteractions);
+            }
+
+            __instance.ContextMenu.Show<EItemInfoButton>(position, itemInfoInteractions, null, itemContext.Item);
+
+            return false; // Skip the original method
+
+
+        }
+
+    }
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace OpenTheDamnContextMenu
+{
+    //first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this code.
+    [BepInPlugin("JD.OpenTheDamnContextMenu", "Open The Damn Context Menu", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+
+        private void Awake()
+        {
+            LogSource = Logger;
+            LogSource.LogInfo("OpenTheDamnContextMenu plugin loaded!");
+
+            new OpenTheContext().Enable();
+        }
+    }
+}
