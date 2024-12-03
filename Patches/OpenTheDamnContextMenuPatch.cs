6a14d700e378e27834191fbbde0cd67b413d7a4e
JD <JD>
2024-12-02 18:20:46 -0800

    updated for 3.10

--- Patches/OpenTheDamnContextMenuPatch.cs
+++ Patches/OpenTheDamnContextMenuPatch.cs
@@ -2,9 +2,10 @@
 using EFT.UI;
 using HarmonyLib;
 using SPT.Reflection.Patching;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
-
 
 namespace OpenTheDamnContextMenu
 {
@@ -16,28 +17,42 @@ namespace OpenTheDamnContextMenu
         }
 
         // Use reflection to get the private fields
-        private static readonly FieldInfo inventoryControllerField = typeof(ItemUiContext).GetField("inventoryControllerClass", BindingFlags.NonPublic | BindingFlags.Instance);
         private static readonly FieldInfo itemInfoInteractionsField = typeof(ItemUiContext).GetField("itemInfoInteractionsAbstractClass", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly FieldInfo dictionaryField = typeof(ItemUiContext).GetField("dictionary_0", BindingFlags.NonPublic | BindingFlags.Instance);
 
         [PatchPrefix]
         static bool Prefix(ItemUiContext __instance, ItemContextAbstractClass itemContext, Vector2 position)
         {
+            // Get the private dictionary field
+            var dictionary = (Dictionary<EItemInfoButton, string>)dictionaryField.GetValue(__instance);
 
-            var inventoryController = (InventoryControllerClass)inventoryControllerField.GetValue(__instance);
+            // Get item interactions
             var itemInfoInteractions = __instance.GetItemContextInteractions(itemContext, null);
 
+            // Set itemInfoInteractions field
             if (itemInfoInteractionsField != null)
             {
                 itemInfoInteractionsField.SetValue(__instance, itemInfoInteractions);
             }
 
-            __instance.ContextMenu.Show<EItemInfoButton>(position, itemInfoInteractions, null, itemContext.Item);
+            // Check and update the AddOffer interaction
+            if (itemInfoInteractions.AllInteractions.Contains(EItemInfoButton.AddOffer))
+            {
+                var ragFair = __instance.Session?.RagFair;
+                if (ragFair != null && ragFair.Available)
+                {
+                    int myOffersCount = ragFair.MyOffersCount;
+                    int maxOffersCount = ragFair.MaxOffersCount;
+                    string text = string.Format("AddOfferButton{0}/{1}".Localized(null), myOffersCount, maxOffersCount);
+                    dictionary[EItemInfoButton.AddOffer] = text;
+                }
+            }
 
-            return false; // Skip the original method
+            // Show the context menu
+            __instance.ContextMenu.Show<EItemInfoButton>(position, itemInfoInteractions, dictionary, itemContext.Item);
 
-
+            // Skip the original method
+            return false;
         }
-
     }
-
 }
